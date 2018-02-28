498fc3b6489d918288ff0448c99fd23d6bb31544
Hiroki Yoshida <aretan@outlook.jp>
2018-02-28 21:06:09 +0900

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_sitemap do
+  name 'Redmine Sitemap plugin'
+  author 'Hiroki Yoshida'
+  description 'Sitemap.xml generater plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/yoshida-mediba/redmine_sitemap'
+  author_url 'https://aretan.jp'
+end
